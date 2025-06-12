acece473c024549558a969b6f82b2bb901d5578d
Onetrippin <kolimpiadnik@gmail.com>
2025-06-12 23:48:42 +0500

    added creating all tools balances for new user

--- src/app/internal/data/repositories/user.py
+++ src/app/internal/data/repositories/user.py
@@ -1,18 +1,24 @@
 from uuid import UUID
 
+from app.internal.data.models.balance import Balance
+from app.internal.data.models.tool import Tool
 from app.internal.data.models.user import User
 from app.internal.domain.interfaces.user import IUserRepository
 
 
 class UserRepository(IUserRepository):
     def create(self, name: str, token_encrypted: str, token_hash: str) -> dict:
-        user, _ = User.objects.get_or_create(
+        user, created = User.objects.get_or_create(
             name=name,
             defaults={
                 'token_encrypted': token_encrypted,
                 'token_hash': token_hash,
             },
         )
+        if created:
+            Balance.objects.bulk_create(
+                [Balance(user=user, tool_id=tool_id, amount=0) for tool_id in Tool.objects.values_list('id', flat=True)]
+            )
         return {'id': user.id, 'name': user.name, 'role': user.role, 'api_key_encrypted': user.token_encrypted}
 
     def get_user_by_token_hash(self, token_hash: str) -> dict | None:
